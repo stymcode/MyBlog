1e7acd1b692aa980da108f8ac6041ea4f2664b49
stymcode <satyamk050302@gmail.com>
2023-12-09 01:02:45 +0530

    first commit

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBpAwH1sUwsfMMLqeT4IYtDQAXcmc7ZTz4",
+    authDomain: "blooging-website-1a641.firebaseapp.com",
+    projectId: "blooging-website-1a641",
+    storageBucket: "blooging-website-1a641.appspot.com",
+    messagingSenderId: "718227732254",
+    appId: "1:718227732254:web:9e183e7c1ea37b27c37b0f"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+const blogSectio = document.querySelector('.blogs-section');
+
+db.collection("blogs").get().then((blogs) => {
+    blogs.forEach(blog => {
+        if(blog.id != decodeURI(location.pathname.split("/").pop())){
+            createBlo(blog);
+        }
+    })
+})
+
+const createBlo = (blog) => {
+    let data = blog.data();
+    blogSectio.innerHTML += `
+    <div class="blog-card">
+        <img src="${data.bannerImage}" class="blog-image" alt="">
+        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+        <a href="/${blog.id}" class="btnn">read</a>
+    </div>
+    `;
+};
